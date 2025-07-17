d3b6b18f86eb531bc82e4ec9ab4a2752c966673e
UjjawalKumar <ujjawal@wizcomtech.com>
2025-07-17 17:40:21 +0530

    first commit
    Signed-off-by: UjjawalKumar <ujjawal@wizcomtech.com>

---
+++ src/components/HomePageElement/ProductArea.js
@@ -0,0 +1,13 @@
+import FilterItems from "../../components/HomePageElement/FilterItems";
+import Products from "../../components/HomePageElement/Products";
+
+function ProductArea(){
+    return(
+         <div className="main-content">
+     <FilterItems />
+     <Products />
+ 
+</div>
+    )
+}
+export default ProductArea;
