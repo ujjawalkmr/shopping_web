9ea3cec9409ea5a074b3cd63295932e0961e3e6e
UjjawalKumar <ujjawal@wizcomtech.com>
2025-08-01 15:18:29 +0530

    color utils add
    Signed-off-by: UjjawalKumar <ujjawal@wizcomtech.com>

--- src/components/HomePageElement/HomePageImage.js
+++ src/components/HomePageElement/HomePageImage.js
@@ -1,15 +1,16 @@
 import farmingImg from "../../assets/images/farming.jpg";
-function HomePageImage(){
-return (
-     <div className="main-img-container">
-  <img src={farmingImg} alt="App Logo" className="main-img" />
-   <div className="main-img-text">
-    <h1>Discover, Buy, Sell. Anything. 🌾</h1>
-    <p>Your ultimate plateform for connecting buyers and sellers worldwide. Expolre endless possibilities and unlock new opportunities</p>
-  <button className="image-button">Start Exploring Today</button>
-  </div>
-</div>
-)
+import Button from "../../commonPage/Buttons/Button";
+function HomePageImage() {
+  return (
+    <div className="main-img-container">
+      <img src={farmingImg} alt="App Logo" className="main-img" />
+      <div className="main-img-text">
+        <h1>Discover, Buy, Sell. Anything. 🌾</h1>
+        <p>Your ultimate plateform for connecting buyers and sellers worldwide. Expolre endless possibilities and unlock new opportunities</p>
+        <Button label={"Start Exploring Today"} className="home-page-image-button" />
+      </div>
+    </div>
+  )
 }
 
 export default HomePageImage;
