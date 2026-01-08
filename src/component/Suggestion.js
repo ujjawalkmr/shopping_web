f3e476dabe685c58cc6cb99fb6b29145f3919018
UjjawalKumar <ujjawal@wizcomtech.com>
2026-01-08 17:58:01 +0530

    suggestion page
    Signed-off-by: UjjawalKumar <ujjawal@wizcomtech.com>

--- src/component/Suggestion.js
+++ src/component/Suggestion.js
@@ -3,6 +3,9 @@ function Suggestion() {
     return (
         <div className="suggestion">
             <h2>Suggestion</h2>
+            <div>
+                <p>suggestion box</p>
+            </div>
         </div>
     );
 }
