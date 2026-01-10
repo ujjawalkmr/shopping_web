5c106484b382b7240692b619171e8b82c51e49c1
Ujjawal Kumar <ujjawalkmr9211@gmail.com>
2026-01-11 01:09:11 +0530

    image and animation add
    Signed-off-by: Ujjawal Kumar <ujjawalkmr9211@gmail.com>

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,26 @@
 import React from "react";
+import "../css_file/About.css";
 
 function About() {
-    return <h1>About Us</h1>;
+    return  <div className="about-container">
+      <img
+        className="about-image"
+        src="/assets/images/field_cow.avif"
+        alt="About"
+      />
+      <div className="about-text">
+    Welcome to Our Farm
+  </div>
+<div className="who-we-are-container">
+    <div className="left-side">
+        <h1>Who we are<br></br></h1>
+        <p>haegdh</p>
+    </div>
+    <div className="right-side">
+        <h3>igchwfhvbhb</h3>
+    </div>
+</div>
+  </div>
 }
 
 export default About;
