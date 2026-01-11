66d57fd1a999624dffc22f6dd022c35cfabd1dbd
Ujjawal Kumar <ujjawalkmr9211@gmail.com>
2026-01-11 23:50:33 +0530

    about text completed
    Signed-off-by: Ujjawal Kumar <ujjawalkmr9211@gmail.com>

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,11 +13,19 @@ function About() {
   </div>
 <div className="who-we-are-container">
     <div className="left-side">
-        <h1>Who we are<br></br></h1>
-        <p>haegdh</p>
+<h1 className="heading">Who we are</h1>
+        <p className="paragraph">We are multi-generational family dairy farm commited 
+          su3 suiactices and susthest quallicifine highest quility milk Located in Farming region 
+          ,our cows graze freely for nutrients-rich fields. Every day we work to 
+          briok to bing he wholesome goodness of fam farm directly to you
+          We are multi-generational family dairy farm commited 
+          su3 suiactices and susthest quallicifine highest quility milk Located in Farming region 
+          ,our cows graze freely for nutrients-rich fields. Every day we work to 
+          briok to bing he wholesome goodness of fam farm directly to you
+        </p>
     </div>
     <div className="right-side">
-        <h3>igchwfhvbhb</h3>
+        <h3>abefihbw</h3>
     </div>
 </div>
   </div>
